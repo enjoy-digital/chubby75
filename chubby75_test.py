0a0b32b9f7f6bc8262a4082d379b01fea7505516
Florent Kermarrec <florent@enjoy-digital.fr>
2018-12-17 15:37:06 +0100

    test target: fix compilation

--- chubby75_test.py
+++ chubby75_test.py
@@ -33,7 +33,7 @@ class _CRG(Module):
         f = Fraction(int(sys_clk_freq), int(f0))
         n, m = f.denominator, f.numerator
         assert f0/n*m == sys_clk_freq
-        p = 8
+        p = 4
         pll_lckd = Signal()
         pll_fb = Signal()
         pll_sys = Signal()
@@ -83,15 +83,15 @@ class EthernetTestSoC(SoCCore):
                                                    sys_clk_freq,
                                                    with_icmp=True)
         self.ethphy.crg.cd_eth_rx.clk.attr.add("keep")
-        self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
+        #self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
         platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, period_ns(125e6))
-        platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, period_ns(125e6))
+        #platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, period_ns(125e6))
         platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.ethphy.crg.cd_eth_rx.clk)
-        platform.add_false_path_constraints(
-            self.crg.cd_sys.clk,
-            self.ethphy.crg.cd_eth_tx.clk)
+        #platform.add_false_path_constraints(
+        #    self.crg.cd_sys.clk,
+        #    self.ethphy.crg.cd_eth_tx.clk)
         platform.add_source("gateware/rgmii_if.vhd")
 
 
