84c29015a5cdbd1acf503823b039f2a0489583c2
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-03 09:22:33 +0100

    test targets: simplify using S6PLL

--- rgmii_test.py
+++ rgmii_test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-from fractions import Fraction
-
 from migen import *
-from migen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
+from litex.soc.cores.clock import S6PLL
 
 from liteeth.phy.s6rgmii import LiteEthPHYRGMII
 from liteeth.core import LiteEthUDPIPCore
@@ -18,47 +16,17 @@ import platform
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
-        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_sys_ps = ClockDomain()
 
-        self.reset = Signal()
+        # # #
 
-        f0 = 25*1000000
         clk25 = platform.request("clk25")
-        clk25a = Signal()
-        self.specials += Instance("IBUFG", i_I=clk25, o_O=clk25a)
-        clk25b = Signal()
-        self.specials += Instance("BUFIO2", p_DIVIDE=1,
-                                  p_DIVIDE_BYPASS="TRUE", p_I_INVERT="FALSE",
-                                  i_I=clk25a, o_DIVCLK=clk25b)
-        f = Fraction(int(sys_clk_freq), int(f0))
-        n, m = f.denominator, f.numerator
-        assert f0/n*m == sys_clk_freq
-        p = 4
-        pll_lckd = Signal()
-        pll_fb = Signal()
-        pll_sys = Signal()
-        self.specials.pll = [
-            Instance("PLL_ADV",
-                p_SIM_DEVICE="SPARTAN6",
-                p_BANDWIDTH="OPTIMIZED", p_COMPENSATION="INTERNAL",
-                p_REF_JITTER=.01, p_CLK_FEEDBACK="CLKFBOUT",
-                i_DADDR=0, i_DCLK=0, i_DEN=0, i_DI=0, i_DWE=0, i_RST=0, i_REL=0,
-                p_DIVCLK_DIVIDE=1, p_CLKFBOUT_MULT=m*p//n, p_CLKFBOUT_PHASE=0.,
-                i_CLKIN1=clk25b, i_CLKIN2=0, i_CLKINSEL=1,
-                p_CLKIN1_PERIOD=1e9/f0, p_CLKIN2_PERIOD=0.,
-                i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb, o_LOCKED=pll_lckd,
-                o_CLKOUT0=pll_sys, p_CLKOUT0_DUTY_CYCLE=.5,
-                p_CLKOUT0_PHASE=0., p_CLKOUT0_DIVIDE=p//1),
-            Instance("BUFG", i_I=pll_sys, o_O=self.cd_sys.clk)
-        ]
-        reset = self.reset
-        self.clock_domains.cd_por = ClockDomain()
-        por = Signal(max=1 << 11, reset=(1 << 11) - 1)
-        self.sync.por += If(por != 0, por.eq(por - 1))
-        self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
-        self.specials += AsyncResetSynchronizer(self.cd_por, reset)
-        self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll_lckd | (por > 0))
-        platform.add_period_constraint(self.cd_sys.clk, 1e9/sys_clk_freq)
+        platform.add_period_constraint(clk25, 1e9/25e6)
+
+        self.submodules.pll = pll = S6PLL(speedgrade=-2)
+        pll.register_clkin(clk25, 25e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
 
 # RGMIITest ----------------------------------------------------------------------------------------
 
--- sdram_test.py
+++ sdram_test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-import argparse
-from fractions import Fraction
-
 from migen import *
-from migen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.soc.integration.soc_sdram import *
 from litex.soc.integration.builder import *
+from litex.soc.cores.clock import S6PLL
 
 from litedram.modules import _TechnologyTimings
 from litedram.modules import _SpeedgradeTimings
@@ -21,60 +18,25 @@ import platform
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
-        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys    = ClockDomain()
         self.clock_domains.cd_sys_ps = ClockDomain()
 
-        self.reset = Signal()
+        # # #
 
-        f0 = 25*1000000
         clk25 = platform.request("clk25")
-        clk25a = Signal()
-        self.specials += Instance("IBUFG", i_I=clk25, o_O=clk25a)
-        clk25b = Signal()
-        self.specials += Instance("BUFIO2", p_DIVIDE=1,
-                                  p_DIVIDE_BYPASS="TRUE", p_I_INVERT="FALSE",
-                                  i_I=clk25a, o_DIVCLK=clk25b)
-        f = Fraction(int(sys_clk_freq), int(f0))
-        n, m = f.denominator, f.numerator
-        assert f0/n*m == sys_clk_freq
-        p = 8
-        pll_lckd = Signal()
-        pll_fb = Signal()
-        pll_sys = Signal()
-        pll_sys_ps = Signal()
-        self.specials.pll = [
-            Instance("PLL_ADV",
-                p_SIM_DEVICE="SPARTAN6",
-                p_BANDWIDTH="OPTIMIZED", p_COMPENSATION="INTERNAL",
-                p_REF_JITTER=.01, p_CLK_FEEDBACK="CLKFBOUT",
-                i_DADDR=0, i_DCLK=0, i_DEN=0, i_DI=0, i_DWE=0, i_RST=0, i_REL=0,
-                p_DIVCLK_DIVIDE=1, p_CLKFBOUT_MULT=m*p//n, p_CLKFBOUT_PHASE=0.,
-                i_CLKIN1=clk25b, i_CLKIN2=0, i_CLKINSEL=1,
-                p_CLKIN1_PERIOD=1e9/f0, p_CLKIN2_PERIOD=0.,
-                i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb, o_LOCKED=pll_lckd,
-                o_CLKOUT0=pll_sys, p_CLKOUT0_DUTY_CYCLE=.5,
-                p_CLKOUT0_PHASE=0., p_CLKOUT0_DIVIDE=p//1,
-                o_CLKOUT1=pll_sys_ps, p_CLKOUT1_DUTY_CYCLE=.5,
-                p_CLKOUT1_PHASE=270., p_CLKOUT1_DIVIDE=p//1),
-            Instance("BUFG", i_I=pll_sys, o_O=self.cd_sys.clk),
-            Instance("BUFG", i_I=pll_sys_ps, o_O=self.cd_sys_ps.clk),
-        ]
-        reset = self.reset
-        self.clock_domains.cd_por = ClockDomain()
-        por = Signal(max=1 << 11, reset=(1 << 11) - 1)
-        self.sync.por += If(por != 0, por.eq(por - 1))
-        self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
-        self.specials += AsyncResetSynchronizer(self.cd_por, reset)
-        self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll_lckd | (por > 0))
-        platform.add_period_constraint(self.cd_sys.clk, 1e9/sys_clk_freq)
-        platform.add_period_constraint(self.cd_sys_ps.clk, 1e9/sys_clk_freq)
+        platform.add_period_constraint(clk25, 1e9/25e6)
 
-        # sdram_clock
+        self.submodules.pll = pll = S6PLL(speedgrade=-2)
+        pll.register_clkin(clk25, 25e6)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=270)
+
         self.specials += Instance("ODDR2",
-            p_DDR_ALIGNMENT="NONE", p_INIT=0, p_SRTYPE="SYNC",
+            p_DDR_ALIGNMENT="NONE",
+            p_INIT=0, p_SRTYPE="SYNC",
             i_D0=0, i_D1=1, i_S=0, i_R=0, i_CE=1,
             i_C0=self.cd_sys.clk, i_C1=~self.cd_sys.clk,
-            o_Q=platform.request("sdram_clock", 0))
+            o_Q=platform.request("sdram_clock"))
 
 # SDRAM Module -------------------------------------------------------------------------------------
 
